3143953d4efe0c52ae09959aa0c9e4ae5028bfd5
Kaviraj <ksb1910.07@gmail.com>
2022-01-22 22:16:47 +0530

    project start again

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import jsonPlaceholder from "../apis/jsonPlaceholder";
-
-export const fetchPosts = () => async (dispatch) => {
-  const res = await jsonPlaceholder.get("/posts");
-  dispatch({ type: "FETCH_POSTS", payload: res });
-};
-
-//bad approach (using this async await method without any middlewares )
-// Actions must be plain objects. Instead, the actual type was: 'Promise'. You may need to add middleware to your store setup to handle dispatching other values, such as 'redux-thunk' to handle dispatching functions.
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-
-export default combineReducers({
-  //dummy reducers to remove store error
-  replaceMe: () => 10,
-});
